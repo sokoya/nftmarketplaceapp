8279a8a67130987f27525932f4e15006a844ab3a
Sokoya Philip <philo4u2c@gmail.com>
2022-05-05 09:19:00 +0100

    first commit

---
+++ components/index.js
@@ -0,0 +1,19 @@
+import HomeHeader from "./HomeHeader";
+import FocusedStatusBar from "./FocusedStatusBar";
+import NFTCard from "./NFTCard";
+import { CircleButton, RectButton } from "./Button";
+import DetailsBid from "./DetailsBid";
+import DetailsDesc from "./DetailsDesc";
+import { SubInfo } from "./SubInfo";
+
+export { 
+    HomeHeader, 
+    NFTCard, 
+    FocusedStatusBar,
+    RectButton,
+    CircleButton,
+    DetailsBid,
+    DetailsDesc,
+    SubInfo
+    
+};
